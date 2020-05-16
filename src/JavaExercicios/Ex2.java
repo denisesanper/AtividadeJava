8d1384929dce8df2dd4c5b271fd01e92d3fc8881
Denise Pereira <denisefeds@hotmail.com>
2020-05-15 22:32:22 -0300

    exercicio 2

---
+++ src/JavaExercicios/Ex2.java
@@ -0,0 +1,13 @@
+package JavaExercicios;
+
+public class Ex2 {
+	public static void main (String[] args) {
+		
+		for(int i=1000;i<2000;i++) {
+			if(i%11==0 || i%11==5) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+}
